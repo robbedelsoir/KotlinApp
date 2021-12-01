f0fa8522f4755f688f7b855890eb2197e1c53e8c
dirkhostens <73101918+dirkhostens@users.noreply.github.com>
2021-12-01 15:11:18 +0100

    Add merge in layout MainActivity

--- app/src/main/java/be/vives/mytodo/MainActivity.kt
+++ app/src/main/java/be/vives/mytodo/MainActivity.kt
@@ -5,17 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation.findNavController
-import be.vives.mytodo.databinding.ActivityMainBinding
-import be.vives.mytodo.models.User
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView( R.layout.activity_main)
 
     }
 
